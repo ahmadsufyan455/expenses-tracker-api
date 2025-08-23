3daf5ef24ec53cc5ae58f594d5f2701ee856b478
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-08-23 11:43:09 +0700

    feat: add database models

---
+++ db/models.py
@@ -0,0 +1,82 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, DateTime, Enum as SQLEnum
+from sqlalchemy.orm import relationship
+from db.database import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from datetime import datetime
+from enum import Enum
+
+class TransactionType(Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
+
+class PaymentMethod(Enum):
+    CASH = "cash"
+    CREDIT_CARD = "credit_card"
+    BANK_TRANSFER = "bank_transfer"
+    DIGITAL_WALLET = "digital_wallet"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    email = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String),
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+
+    categories = relationship("Category", back_populates="user", cascade="all, delete-orphan")
+    transactions = relationship("Transaction", back_populates="user", cascade="all, delete-orphan")
+    budgets = relationship("Budget", back_populates="user", cascade="all, delete-orphan")
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    name = Column(String)
+
+    user = relationship("User", back_populates="categories")
+    transactions = relationship("Transaction", back_populates="category")
+    budgets = relationship("Budget", back_populates="category")
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    amount = Column(Integer)
+    type = Column(SQLEnum(TransactionType), nullable=False)
+    payment_method = Column(SQLEnum(PaymentMethod), nullable=False)
+    description = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+
+    user = relationship("User", back_populates="transactions")
+    category = relationship("Category", back_populates="transactions")
+    attachments = relationship("Attachment", back_populates="transaction", cascade="all, delete-orphan")
+
+class Budget(Base):
+    __tablename__ = "budgets"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    amount = Column(Integer)
+    month = Column(Date, nullable=False)
+
+    user = relationship("User", back_populates="budgets")
+    category = relationship("Category", back_populates="budgets")
+
+class Attachment(Base):
+    __tablename__ = "attachments"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    transaction_id = Column(UUID(as_uuid=True), ForeignKey("transactions.id"))
+    file_path = Column(String)
+    uploaded_at = Column(DateTime, default=datetime.now)
+
+    transaction = relationship("Transaction", back_populates="attachments")
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from db.database import engine
+from db.models import Base
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
